73dca0be407265d25b3fbde7302f9b9c4f21a4ae
Jack Jain <cirocelot@gmail.com>
2019-01-06 23:10:44 -0700

    Made the onClick funnier

--- react-app1/src/index.js
+++ react-app1/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
 	render() {
 		return (
-			<button className="square">
+			<button className="square" onClick={function() {alert('Hooray! You clicked a box!!');}}>
 			{this.props.value}
 			</button>
 	);
