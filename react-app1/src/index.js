02506fe3e1e83ee914b690f238c1a68bee513dba
Jack Jain <cirocelot@gmail.com>
2019-02-01 11:10:31 -0700

    construtor method in Square subclass

--- react-app1/src/index.js
+++ react-app1/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: null,
+		};
+	}
+
 	render() {
 		return (
-			<button className="square" onClick={() => this.props.onClick ({value:'👌'})}>
-			{this.props.value}
+			<button className="square" onClick={() => this.setState({value:'👌'})}>
+			{this.state.value}
 			</button>
 	);
 	}
