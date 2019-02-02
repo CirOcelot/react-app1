4fd65e866030262262d81e1c1c4e25034eb531bb
Jack Jain <cirocelot@gmail.com>
2019-02-02 11:23:30 -0700

    Fixed onClicks

--- react-app1/src/index.js
+++ react-app1/src/index.js
@@ -2,21 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: null,
-		};
-	}
-
-	render() {
+function Square(props) {
 		return (
-			<button className="square" onClick={() => this.setState({value:'👌'})}>
-			{this.state.value}
+			<button className="square" onClick={props.onclick}>
+			{props.value}
 			</button>
 	);
-	}
 }
 
 class Board extends React.Component {
@@ -48,21 +39,21 @@ class Board extends React.Component {
 		return (
 			<div>
 			<div className="status">{status}</div>
-			<div className="board-row">
+		<div className="board-row">
 			{this.renderSquare(0)}
-		{this.renderSquare(1)}
-		{this.renderSquare(2)}
-	</div>
+			{this.renderSquare(1)}
+			{this.renderSquare(2)}
+		</div>
 		<div className="board-row">
 			{this.renderSquare(3)}
-		{this.renderSquare(4)}
-		{this.renderSquare(5)}
-	</div>
+			{this.renderSquare(4)}
+			{this.renderSquare(5)}
+		</div>
 		<div className="board-row">
 			{this.renderSquare(6)}
-		{this.renderSquare(7)}
-		{this.renderSquare(8)}
-	</div>
+			{this.renderSquare(7)}
+			{this.renderSquare(8)}
+		</div>
 		</div>
 	);
 	}
